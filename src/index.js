e7b0fd212d5237a9a59b78de7df4f920ee981de9
PrenyaNyan <alexandrepereon8@gmail.com>
2023-05-31 11:10:06 +0000

    Add fire effect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import initTyped from "./scripts/typed";
+import "./scripts/fireEffect";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initTyped();
